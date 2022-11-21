678ca737776084ca509b4a8c69e7ae3791eec264
Andrew Kramer <andrewkramer@google.com>
2022-11-21 15:48:42 -0500

    Updates

--- code/2_load_data.sh
+++ code/2_load_data.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#Download Synthea data
+wget https://synthetichealth.github.io/synthea-sample-data/downloads/synthea_sample_data_fhir_r4_sep2019.zip
+
+#Unzip the data
+unzip synthea_sample_data_fhir_r4_sep2019.zip
+
 #Copy Data to GCS bucket
-gsutil cp data/resources.ndjson gs://<your GCS bucket>
+gsutil -m cp -r fhir gs://<your GCS bucket>
 
 #Import Data
 gcloud healthcare fhir-stores import gcs quickstartFHIR \
-	--gcs-uri=gs://<your GCS bucket> \
+	--gcs-uri=gs://<your GCS bucket>/fhir/* \
 	--dataset=quickstartDataset \
-	--content-structure=RESOURCE
+	--content-structure=bundle-pretty
